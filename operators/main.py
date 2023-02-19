7df431a4154ae8a26a355ae20c9d3a55c45a5553
Lonnie1992 <122544791+Lonnie1992@users.noreply.github.com>
2023-02-19 09:26:19 +0100

    Add files via upload

---
+++ operators/main.py
@@ -0,0 +1,24 @@
+# Do not modify these lines
+__winc_id__ = 'd0d3cdcefbb54bc980f443c04ab3a9eb'
+__human_name__ = 'operators'
+
+# Add your code after this line
+language_spoken_spain = 'spanish'
+language_spoken_switzerland = 'german'
+print(language_spoken_spain == language_spoken_switzerland)
+religion_spain = 'Roman Catholic'
+religion_switzerland = 'Roman Catholic'
+print(religion_spain == religion_switzerland)
+capital_city_spain = 'Madrid'
+capital_city_switzerland = 'Bern'
+print(len(capital_city_spain) != len(capital_city_switzerland))
+gdp_spain = 1798000000000
+gdp_switserland = 618228000000
+print(gdp_switserland > gdp_spain)
+population_growth_spain = 0.13
+population_growth_switzerland = 0.65
+print(population_growth_switzerland and population_growth_spain < 1)
+population_spain = 47163418
+population_switzerland = 8508698
+print(bool(population_spain or population_switzerland < 10000000))
+print(bool(population_spain and population_switzerland < 10000000))
